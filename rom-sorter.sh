7451e74da778f4f711acd0af0f0bda54a7c4325b
AndyHazz <517731+AndyHazz@users.noreply.github.com>
2021-08-15 09:44:28 +0100

    trim trim

--- rom-sorter.sh
+++ rom-sorter.sh
@@ -154,8 +154,8 @@ fi
 pikeyd165_start "yesno" "0.5"
 
 joy2key_start "yesno"
-dialog --trim --title "$SCRIPT_TITLE" \
-  --yesno "Are you ready?\n\n
+dialog --title "$SCRIPT_TITLE" \
+  --yesno --trim "Are you ready?\n\n
   This script will use all killer no filler lists to select best games for each genre and move them into folders for PinHP.\n
   Any existing rom folders will be replaced/updated." 15 30
 response=$?
